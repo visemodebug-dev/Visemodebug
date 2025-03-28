f2a7e59c003ee2b86862587bfa6bba4afd041e83
Chakolito <ckhyle27@gmail.com>
2025-03-28 20:58:31 +0800

    onnx

---
+++ VisemoServices/Controllers/EmotionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisemoServices.Services;
+using System.Threading.Tasks;
+
+
+namespace VisemoServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmotionController : ControllerBase
+    {
+        private readonly IEmotionServices _emotionServices;
+        public EmotionController(IEmotionServices emotionServices)
+        {
+            _emotionServices = emotionServices;
+        }
+      
+
+        [HttpPost("predict-image")]
+        public async Task<IActionResult> PredictImage([FromForm] IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0)
+                return BadRequest("Invalid image file.");
+
+            try
+            {
+                var output = await _emotionServices.PredictImageAsync(imageFile);
+                return Ok(output);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
--- VisemoServices/Program.cs
+++ VisemoServices/Program.cs
@@ -1,15 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using VisemoServices.Services;
 using VisemoServices.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// CORS enabler
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Visemo API", Version = "v1" });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -22,6 +33,8 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 
 //Enable Dependency Injection
 builder.Services.AddScoped<IUserServices, UserServices>();
+// Register IOnnxService as a Singleton
+builder.Services.AddScoped<IEmotionServices, EmotionServices>();
 
 var app = builder.Build();
 
@@ -29,8 +42,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Visemo API v1"));
 }
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
