2310bc1f53dccedbad48055f27115e077bee4d5d
Chakolito <ckhyle27@gmail.com>
2025-07-04 18:43:38 +0800

    Revert "update database types"
    This reverts commit b69a66bcddc724b223b5cd97dacc58fb25f6e3a2.

--- VisemoServices/Model/Activity.cs
+++ VisemoServices/Model/Activity.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace VisemoServices.Model
+﻿namespace VisemoServices.Model
 {
     public class Activity
     {
         public int Id { get; set; }
-        [MaxLength(255)]
         public string Name { get; set; }
         public TimeSpan Timer { get; set; }  // Duration of the activity
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
--- VisemoServices/Model/Classroom.cs
+++ VisemoServices/Model/Classroom.cs
@@ -9,8 +9,7 @@ namespace VisemoServices.Model
             Users = new List<User>();
         }
         public int Id { get; set; }
-        [MaxLength(255)]
-        public string className { get; set; }
+        public string? className { get; set; }
 
         // Foreign key to User and Activity
         public List<User> Users { get; set; } = new List<User>();
