91dad5deaeed764781092fd26560765adc96de2f
Chakolito <ckhyle27@gmail.com>
2025-07-04 18:45:34 +0800

    Reapply "update database types"
    This reverts commit 2310bc1f53dccedbad48055f27115e077bee4d5d.

--- VisemoServices/Model/Activity.cs
+++ VisemoServices/Model/Activity.cs
@@ -1,8 +1,11 @@
-﻿namespace VisemoServices.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisemoServices.Model
 {
     public class Activity
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public string Name { get; set; }
         public TimeSpan Timer { get; set; }  // Duration of the activity
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
